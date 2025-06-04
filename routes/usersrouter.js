c5948b1ac7ce131d3f71c138ee031ab579d21eef
amritansh-mishra <mamritansh1911@gmail.com>
2025-06-04 17:02:37 +0530

    user models

--- routes/usersrouter.js
+++ routes/usersrouter.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require('../models/user-models');
 
 router.get("/", (req, res) => { 
     res.send("User Home Page");
 });
+router.post('/register', async (req, res) => {
+    try{
+    let { fullname, email, password} = req.body;
+
+    
+    let user = await userModel.create({
+        fullname,
+        email,
+        password
+    });
+    res.send(user);}
+    catch(err) {
+        console.error(err);
+        res.status(500).send("Error creating user");
+    }
+});
 
 module.exports = router;
+
